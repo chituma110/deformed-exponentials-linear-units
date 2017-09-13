4562a0ead15be9b20a17f51c461b916011c048c8
chituma110 <1306524625@qq.com>
2017-09-13 19:25:31 +0800

    edit source file

--- pnelu_layer.hpp
+++ pnelu_layer.hpp
@@ -11,39 +11,6 @@
 
 namespace caffe {
 
-
-template <typename Dtype>
-class PNELULayer : public NeuronLayer<Dtype> {
- public:
-  explicit PNELULayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
-
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-
-  virtual inline const char* type() const { return "PNELU"; }
-
- protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-
-  bool channel_shared_;
-  Dtype t;
-  Blob<Dtype> multiplier_;  // dot multiplier for backward computation of params
-  Blob<Dtype> backward_buff_;  // temporary buffer for backward computation
-  Blob<Dtype> bottom_memory_;  // memory for in-place computation
-};
-
 }  // namespace caffe
 
 #endif  // CAFFE_PNELU_LAYER_HPP_
